b126d93b3b4d0b91e0e0290e8c6458156ec1ea1a
Graeme Worthy <graemeworthy@gmail.com>
2010-11-19 13:20:29 -0800

    bundler, i think i need it

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'http://rubygems.org'
+gem 'sinatra', '~>1.1.0'
+gem 'erubis', '~>2.6.6'
+
--- amigreen.rb
+++ amigreen.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'sinatra' 
-require 'erb'
+require 'erubis'
 
 @@apps = {}
 
